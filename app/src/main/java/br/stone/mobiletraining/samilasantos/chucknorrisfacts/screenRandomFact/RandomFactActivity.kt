01b18feb19822b1567111eed2b6616b99f60393c
Samila Ruane <samilaruane@gmail.com>
2018-07-31 18:46:36 -0300

    Adding random fact screen layout.

---
+++ app/src/main/java/br/stone/mobiletraining/samilasantos/chucknorrisfacts/screenRandomFact/RandomFactActivity.kt
@@ -0,0 +1,36 @@
+package br.stone.mobiletraining.samilasantos.chucknorrisfacts.screenRandomFact
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import br.stone.mobiletraining.samilasantos.chucknorrisfacts.R
+import com.jakewharton.rxbinding2.view.clicks
+import kotlinx.android.synthetic.main.activity_random_fact.*
+
+class RandomFactActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_random_fact)
+
+        defineIntents()
+        setupView()
+    }
+
+    private fun defineIntents() {
+        icon_search
+            .clicks()
+            .subscribe { }
+
+        icon_share
+            .clicks()
+            .subscribe { }
+
+        icon_update
+            .clicks()
+            .subscribe { }
+    }
+
+    private fun setupView() {
+
+    }
+}
