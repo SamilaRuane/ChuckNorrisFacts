c8391527158b35cbce064e1fd7a3bea3784af58c
Samila Ruane <samilaruane@gmail.com>
2018-08-02 18:41:28 -0300

    Fixing error handlers issues.

--- data/src/main/java/br/stone/mobiletraining/samilasantos/data/service/common/ChuckNorrisService.kt
+++ data/src/main/java/br/stone/mobiletraining/samilasantos/data/service/common/ChuckNorrisService.kt
@@ -1,5 +1,6 @@
 package br.stone.mobiletraining.samilasantos.data.service.common
 
+import android.util.Log
 import br.stone.mobiletraining.samilasantos.domain.common.Fact
 import br.stone.mobiletraining.samilasantos.domain.randomFact.RandomFactRepository
 import io.reactivex.Single
@@ -12,5 +13,6 @@ class ChuckNorrisService(private val api: ChuckNorrisApi) : RandomFactRepository
         .compose(HandleConnectionErrors())
         .compose(HandleParsingErrors())
         .compose(HandleHttpErrors())
+        .doOnError {  Log.i("TRACKER", "Manda para o dominio $it") }
         .map { fact -> Fact(fact.id, fact.value, fact.url, fact.category?.first() ?: "Uncategorized") }
 }
