f8cdf70cda8981aa4a246a0afbdb6c1ddebbaf67
Samila Ruane <samilaruane@gmail.com>
2018-08-03 13:40:27 -0300

    Adding data layer (#5)
    * Adding data layer.
    
    * Adding error states to the view.
    
    * Adding unit tests to data layer.

---
+++ data/src/test/java/br/stone/mobiletraining/samilasantos/data/service/MockWebServerUtils.kt
@@ -0,0 +1,46 @@
+package br.stone.mobiletraining.samilasantos.data.service
+
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+
+object MockWebServerUtils {
+
+    fun runWithMockServerErrorResponse(
+        errorCode: Int,
+        testBody: MockWebServer.(baseUrl: String) -> Unit
+    ) =
+        runWithMockServer({
+            enqueue(
+                MockResponse()
+                    .setResponseCode(errorCode)
+            )
+        }, testBody)
+
+    fun runWithMockServerOkResponse(
+        body: String,
+        returnCode: Int = 200,
+        testBody: MockWebServer.(baseUrl: String) -> Unit
+    ) =
+        runWithMockServer({ enqueueOkResponse(body, returnCode) }, testBody)
+
+    private fun runWithMockServer(
+        enqueueBody: MockWebServer.() -> Unit,
+        testBody: MockWebServer.(baseUrl: String) -> Unit
+    ) {
+        MockWebServer().use { webServer ->
+            enqueueBody.invoke(webServer)
+
+            webServer.start()
+
+            testBody.invoke(webServer, webServer.url("/").toString())
+        }
+    }
+
+    private fun MockWebServer.enqueueOkResponse(body: String, returnCode: Int) =
+        enqueue(
+            MockResponse()
+                .setResponseCode(returnCode)
+                .setHeader("content-type", "application/json")
+                .setBody(body)
+        )
+}
