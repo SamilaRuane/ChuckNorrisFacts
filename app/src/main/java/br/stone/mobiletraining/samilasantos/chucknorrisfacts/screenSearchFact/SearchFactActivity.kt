66ca48cc9c17553ac6f11e14993ef5aa06cf26d1
Samila Ruane <samilaruane@gmail.com>
2018-08-04 20:27:33 -0300

    Adding activity main methods.

--- app/src/main/java/br/stone/mobiletraining/samilasantos/chucknorrisfacts/screenSearchFact/SearchFactActivity.kt
+++ app/src/main/java/br/stone/mobiletraining/samilasantos/chucknorrisfacts/screenSearchFact/SearchFactActivity.kt
@@ -11,9 +11,25 @@ class SearchFactActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_fact)
+    }
 
+    override fun onStart() {
+        super.onStart()
+        defineIntents()
+        setupView()
+    }
+
+    private fun defineIntents() {
+        imageIconSearch.setOnClickListener {}
+    }
+
+    private fun setupView() {
         recyclerFactsList.layoutManager =
             LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
         recyclerFactsList.adapter = FactAdapter(getMockedFacts())
+    }
+
+    override fun onStop() {
+        super.onStop()
     }
 }
