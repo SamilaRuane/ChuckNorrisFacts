64a6c500ad076f344b9155695321633b96759a94
Samila Ruane <samilaruane@gmail.com>
2018-08-06 20:01:11 -0300

    Adding random fact test classes that for some mysterious reason disappeared.

---
+++ data/src/test/java/br/stone/mobiletraining/samilasantos/data/randomFact/RandomFactMother.kt
@@ -0,0 +1,32 @@
+package br.stone.mobiletraining.samilasantos.data.service.randomFact
+
+import br.stone.mobiletraining.samilasantos.domain.common.Fact
+
+object RandomFactMother {
+
+    val successBodyObject = Fact(
+        id = "0PlGg24YSX--haND5nj4Tw",
+        description = "Chuck Norris ride into town on Friday, stayed three Nights, the rode out again on Friday. On a horse named Steve.",
+        url = "https://api.chucknorris.io/jokes/0PlGg24YSX--haND5nj4Tw",
+        category = "Uncategorized"
+    )
+    val successbody = """
+        {
+           "category":null,
+           "icon_url":"https:\/\/assets.chucknorris.host\/img\/avatar\/chuck-norris.png",
+           "id":"0PlGg24YSX--haND5nj4Tw",
+           "url":"https:\/\/api.chucknorris.io\/jokes\/0PlGg24YSX--haND5nj4Tw",
+           "value":"Chuck Norris ride into town on Friday, stayed three Nights, the rode out again on Friday. On a horse named Steve."
+        }
+    """.trimIndent()
+
+    val malFormedBody = """
+        {[jdskjdsf
+           "category":null,
+           "icon_url":"https:\/\/assets.chucknorris.host\/img\/avatar\/chuck-norris.png",
+           "id":"0PlGg24YSX--haND5nj4Tw",
+           "url":"https:\/\/api.chucknorris.io\/jokes\/0PlGg24YSX--haND5nj4Tw",
+           "value":"Chuck Norris ride into town on Friday, stayed three Nights, the rode out again on Friday. On a horse named Steve."
+        }
+    """.trimIndent()
+}
