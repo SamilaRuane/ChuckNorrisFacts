695e11cce6fa52e5bd8a2e12605c35359cd4d58a
Samila Ruane <samilaruane@gmail.com>
2018-08-07 19:38:13 -0300

    Removing const val.

--- domain/src/main/java/br/stone/mobiletraining/samilasantos/domain/searchFact/uc/ProcessCategoryBgColor.kt
+++ domain/src/main/java/br/stone/mobiletraining/samilasantos/domain/searchFact/uc/ProcessCategoryBgColor.kt
@@ -2,11 +2,9 @@ package br.stone.mobiletraining.samilasantos.domain.searchFact.uc
 
 import io.reactivex.Single
 
-const val UNCATEGORIZED = "Uncategorized"
-
 class ProcessCategoryBgColor {
     fun using(category: String): Single<String> =
-        if (category == UNCATEGORIZED) Single.just(Color.GRAY.hex) else Single.just(Color.BLUE.hex)
+        if (category == "Uncategorized") Single.just(Color.GRAY.hex) else Single.just(Color.BLUE.hex)
 }
 
 enum class Color(val hex: String) {
