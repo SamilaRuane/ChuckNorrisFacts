6190bcbedda0d74157439cc0741c867d5af01ad3
Samila Ruane <samilaruane@gmail.com>
2018-08-07 19:20:52 -0300

    Renaming uc responsible for calculate fact description font size.

--- domain/src/main/java/br/stone/mobiletraining/samilasantos/domain/searchFact/uc/CalculateFactDescriptionFontSize.kt
+++ domain/src/main/java/br/stone/mobiletraining/samilasantos/domain/searchFact/uc/CalculateFactDescriptionFontSize.kt
@@ -2,7 +2,7 @@ package br.stone.mobiletraining.samilasantos.domain.searchFact.uc
 
 import io.reactivex.Single
 
-class CalculateFontSize {
+class CalculateFactDescriptionFontSize {
     fun using(descritionSize: Int): Single<Int> =
         if (descritionSize > 50) Single.just(14) else Single.just(16)
 }
